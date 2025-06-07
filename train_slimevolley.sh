07bec0d1d298953bf4c0ff9e44af0c9e2d34816c
Bartek Cupiał <bartlomiej.cupial@gmail.com>
2025-06-07 19:17:54 +0200

    update

--- train_slimevolley.sh
+++ train_slimevolley.sh
@@ -4,13 +4,15 @@ python train.py \
     "neat.pop_size=300" \
     "neat.prob_add_node=0.05" \
     "neat.prob_add_connection=0.15" \
-    "neat.compatibility_threshold=0.3" \
+    "neat.compatibility_threshold=3.0,2.0,1.0,0.5" \
     "neat.survival_threshold=0.25" \
+    "neat.activation_function=[relu,square,sin,tanh,gaussian,identity]" \
     "trainer.n_repeats=16" \
     "trainer.n_evaluations=100" \
     "trainer.max_iter=2000" \
     "trainer.test_interval=50" \
     "trainer.log_interval=10" \
-    "trainer.backprop_steps=100" \
+    "trainer.backprop_steps=1000" \
     "trainer.learning_rate=0.01" \
-    "trainer.l2_penalty=0.001"
+    "trainer.l2_penalty=0.001" \
+    -m
--- train_spiral.sh
+++ train_spiral.sh
@@ -3,8 +3,8 @@ python train.py \
     "neat.pop_size=150" \
     "neat.prob_add_node=0.05" \
     "neat.prob_add_connection=0.15" \
-    "neat.compatibility_threshold=1.5" \
-    "neat.activation_function=relu" \
+    "neat.compatibility_threshold=3.0" \
+    "neat.activation_function=[relu,square,sin,cos]" \
     "trainer.max_iter=100" \
     "trainer.backprop_steps=1000" \
     "trainer.learning_rate=0.03" \
