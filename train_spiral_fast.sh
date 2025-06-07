07cdc4a302540aa2becf8597ab41ee5ef75e0fbd
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2025-06-07 16:42:56 +0100

    train spiral update

--- train_spiral_fast.sh
+++ train_spiral_fast.sh
@@ -4,8 +4,8 @@ python train.py \
     "neat.prob_add_node=0.05" \
     "neat.prob_add_connection=0.15" \
     "neat.compatibility_threshold=1.5" \
-    "neat.activation_function=relu" \
-    "trainer.max_iter=100" \
+    "neat.activation_function=[relu,square,sin,cos]" \
+    "trainer.max_iter=20" \
     "trainer.backprop_steps=1000" \
     "trainer.learning_rate=0.03" \
     "trainer.l2_penalty=0.000"
